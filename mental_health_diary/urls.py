0c9956263f3a737c3bc6ce50c1adfe17e699f3b7
2469916m <2469916m@student.gla.ac.uk>
2022-11-03 19:17:44 +0000

    3. Added blank pages for all non-user-specific pages, added a navigation bar.

--- mental_health_diary/urls.py
+++ mental_health_diary/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('disclaimer/', views.disclaimer, name='disclaimer'),
+    path('self-care/', views.self_help, name='self_help'),
+    path('settings/', views.settings, name='settings'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ]
