011b780b429c2aa79cc6c2d2efa1c62b2e3360db
2469916m <2469916m@student.gla.ac.uk>
2022-11-06 16:24:22 +0000

    4. Added register, login and logout pages

--- mental_health_diary/urls.py
+++ mental_health_diary/urls.py
@@ -1,14 +1,18 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('admin/', admin.site.urls),
     path('about/', views.about, name='about'),
     path('disclaimer/', views.disclaimer, name='disclaimer'),
     path('self-care/', views.self_help, name='self_help'),
     path('settings/', views.settings, name='settings'),
     path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='main/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='main/logout.html'), name='logout'),
+
 ]
